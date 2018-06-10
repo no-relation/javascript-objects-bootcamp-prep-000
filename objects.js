f9c82d7c462b10d94ea481dda650537c961409d3
Eddie C <eddiec76@gmail.com>
2018-06-10 20:24:39 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+  Beatles: "Here Comes the Sun",
+  DaftPunk: "Technologic",
+  Guster: "Demons"
+};
+
+function updatePlaylist(playlist,artist,song) {
+  playlist[artist] = song;
+}
+
